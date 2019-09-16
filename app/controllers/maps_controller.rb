a21534d510b01f4ac0d82b19abf02f9b8f52cda9
harukahorii <annzuwatanukidayo@ezweb.ne.jp>
2019-09-16 16:05:41 +0900

    Update application.js

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -5,6 +5,11 @@ class MapsController < ApplicationController
   # GET /maps.json
   def index
     @maps = Map.all
+    @hash = Gmaps4rails.build_markers(@maps) do |map, marker|
+    marker.lat map.latitude
+    marker.lng map.longitude
+    marker.infowindow map.name
+  end
   end
 
   # GET /maps/1
